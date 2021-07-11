5cb0565364d60e52e731ec05a1dc9db2b8e3ba46
Laeticia Ndutiye <ndutilae@gmail.com>
2021-07-11 16:35:48 -0400

    human body quiz

---
+++ questions.js
@@ -0,0 +1,40 @@
+var questions = [
+    {
+        title: "What percentage of the human body is water?",
+        choices: ["50%", "66%", "75%", "80%"],
+        answer: "66%"
+    },
+
+    {
+        title: "A human's thumb is as long as his what?",
+        choices: ["nose", "eyebrow", "ears", "tongue"],
+        answer: "nose"
+    },
+
+    {
+        title: "A sneeze occurs at about how many miles an hour?",
+        choices: ["50 miles an hour", "75 miles an hour", "100 miles an hour", "200 miles an hour"],
+        answer: "100 miles an hour"
+    },
+
+    {
+        title: "The smallest bone in the human body is about the size of?",
+        choices: ["the head of a pin", 
+                "an eyelash",
+                "a grain of sand",
+                "a grain of rice"],
+        answer: "a grain of rice"
+    },
+
+    {
+        title: "The smallest bones in the human body are found in the?",
+        choices: ["feet", "ears", "fingers", "knees"],
+        answer: "ears"
+    },
+
+    {
+        title: "How many taste buds are on your tongue?",
+        choices: ["one thousand", "five thousand", "nine thousand", "nine million"],
+        answer: "nine thousand"
+    },
+];
